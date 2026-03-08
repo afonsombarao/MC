034e1ba220e3417b1f080fc500c4d5ca48ea7a56
afonsombarao <afonso.j.m.barao@gmail.com>
2026-03-08 01:04:54 +0000

    Update metodo2.m
    comentacao

--- exercicio1/metodo2.m
+++ exercicio1/metodo2.m
@@ -1,9 +1,11 @@
 function [xn1] = metodo2(epsilon, x0)
-xn = x0;
+
+xn = x0; % atribuicao da variavel iterativa
 g = @(x) x.^4/10 + sin(4*x); 
-Psi = @(x) (x.*g(g(x))-g(x).^2)./(g(g(x))-2*g(x)+x);
+Psi = @(x) (x.*g(g(x))-g(x).^2)./(g(g(x))-2*g(x)+x); % funcao iteradora do metodo 2
 xn1 = Psi(xn);
 
+% ciclo que calcula o proximo ponto ate ter |xn1 - xn|<= epsilon
 while abs(xn - xn1) > epsilon
     xn = xn1;
     xn1 = Psi(xn);
