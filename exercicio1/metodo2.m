eddf0e611b9a27627d2876f2e0df3e0c578ef958
afonsombarao <afonso.j.m.barao@gmail.com>
2026-03-08 01:05:25 +0000

    Update metodo2.m

--- exercicio1/metodo2.m
+++ exercicio1/metodo2.m
@@ -1,7 +1,7 @@
 function [xn1] = metodo2(epsilon, x0)
 
 xn = x0; % atribuicao da variavel iterativa
-g = @(x) x.^4/10 + sin(4*x); 
+g = @(x) x.^4/10 + sin(4*x); % funcao que queremos aproximar os pontos fixos
 Psi = @(x) (x.*g(g(x))-g(x).^2)./(g(g(x))-2*g(x)+x); % funcao iteradora do metodo 2
 xn1 = Psi(xn);
 
