babddd5d4ab6321cc3302384b0ad746daf1ac314
afonsombarao <afonso.j.m.barao@gmail.com>
2026-03-08 00:30:54 +0000

    Create metodonewton.m
    criei um ficheiro com uma função auxiliar do método de newton classico

---
+++ exercicio1/metodonewton.m
@@ -0,0 +1,11 @@
+function [xn1] = metodonewton(epsilon,x0)
+xn = x0;
+f = @(x) x.^4/10 + sin(4*x) - x;
+dfdx = @(x) 2*(x.^3)/5 + 4*cos(4*x) - 1;
+h = @(x) x - f(x)./dfdx(x);
+xn1 = h(xn);
+
+while abs(xn1 - xn) > epsilon
+    xn = xn1;
+    xn1 = h(xn);
+end
