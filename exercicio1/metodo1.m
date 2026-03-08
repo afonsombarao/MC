0894661f090f2cc10cc7465841b089d60697abfd
afonsombarao <afonso.j.m.barao@gmail.com>
2026-03-08 01:03:37 +0000

    Update metodo1.m
    comentacao

--- exercicio1/metodo1.m
+++ exercicio1/metodo1.m
@@ -1,10 +1,12 @@
 function [xn1] = metodo1(epsilon,x0)
-xn = x0;
-f = @(x) x.^4/10 + sin(4*x) - x;
+
+xn = x0; % atribuicao da variavel iterativa
+f = @(x) x.^4/10 + sin(4*x) - x; % funcao que queremos aproximar os zeros
 dfdx = @(x) 2*(x.^3)/5 + 4*cos(4*x) - 1;
-h = @(x) x - f(x)./dfdx(x0);
+h = @(x) x - f(x)./dfdx(x0); % funcao iterativa do metodo de newton
 xn1 = h(xn);
 
+% ciclo que calcula iterativamente o proximo ponto ate ter |xn1 - xn|<= epsilon
 while abs(xn1 - xn) > epsilon
     xn = xn1;
     xn1 = h(xn);
