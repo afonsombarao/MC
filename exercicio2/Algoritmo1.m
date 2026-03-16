51f9df2f5122363d950086798feb64be1dc40682
alicegtedim <agtedim@gmail.com>
2026-03-16 22:28:05 +0000

    Add files via upload

---
+++ exercicio2/Algoritmo1.m
@@ -0,0 +1,38 @@
+function [x,t,iter] = Algoritmo1(x1,x2,eps,k)
+
+t=".";
+a=[0;0];
+x=[x1;x2];
+j=@(p,q)([4.*p.^3+4.*p.*q-42.*p+2.*q.^2-14;4.*q.^3+4.*p.*q-26.*q+2.*p.^2-22]);
+h=@(p,q)([12.*p.^2+4.*q-42 4.*p+4.*q;4.*p+4.*q 12.*q.^2+4.*p-26]);
+A=h(x1,x2);
+
+for iter=1:k
+    if norm(x-a,inf)<eps
+        A=h(x(1),x(2));
+        if det(A)>0
+                if trace(A)>0
+                    t="minimo";
+                else
+                    t="maximo";
+                end    
+            elseif det(A)<0
+                t="pontodesela";
+            else
+                t="indeterminado";
+        end
+        return;
+       
+    else
+        a=x;
+        s=-((inv(A))*j(x(1),x(2)));
+
+        x=x-s;
+        y=j(x(1),x(2))-j(a(1),a(2));
+
+        A=A+(((y-A*s)*((y-(A*(s)))'))/((y-A*(s))'*s));
+        J=j(x(1),x(2));
+     
+    end
+
+end
