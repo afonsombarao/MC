fab964e073ffb544963bf66a8b08550bda00aaa7
afonsombarao <afonso.j.m.barao@gmail.com>
2026-03-17 00:53:28 +0000

    Update metodo2eps.m

--- exercicio1/metodo2eps.m
+++ exercicio1/metodo2eps.m
@@ -1,4 +1,4 @@
-function [xn, A] = metodo2eps(k, x0)
+function [xn, A] = metodo2eps(epsilon, x0)
 xn = x0;
 g = @(x) x.^4/10 + sin(4*x); 
 Psi = @(x) (x.*g(g(x))-g(x).^2)./(g(g(x))-2*g(x)+x);
@@ -8,5 +8,5 @@ A = [xn,xn1]
 while abs(xn1 - xn) > epsilon
     xn = xn1;
     xn1 = h(xn);
-    A = [A,xn1]
+    A = [A,xn1];
 end
